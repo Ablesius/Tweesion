5ce7625e6bd871468ce34f2253736728d85811c9
Markus Schmidl <markus.schmidl@ymail.com>
2015-10-17 01:21:00 +0200

    simple url encoding

--- php/main.php
+++ php/main.php
@@ -1,38 +1,30 @@
 <?php
+$fishi_str = $_POST["grade"];
+$whoami = $_POST["p_m"]; // true == premium || false == 08/15
 
-$fishi_str = string($_POST["grade"]);	// der string 
-$whoami = $_POST["p_m"];		// true == premium || false == 08/15
-
-$req_start = "https://api.twitter.com/1.1/";
-
+$req_start = "https://api.twitter.com/1.1/search/tweets.json?q=";
+$fishi_str = data_proc($fishi_str);
+echo $fishi_str;
 if ($whoami)
 {
 	//premium
-		
+
 }
 else
 {
 	// 08/15
-	
+
 }
 
-function pre_pro()	// input = string || output = time_span; language (default english); 
-{
+function data_proc($fishi_str) {
 	// calculate time span based on tweets length
-	$time_span = intval(40*log(strlen($fishi_str), 10)++);
+	$time_span = intval(40*log(strlen($fishi_str), 10)+1);
+	// calculate since for twitter api
+	$since = date('Y-m-d', strtotime('-'.$time_span.'day'));
 
 	// make url encoded query
-	$string = array(explode(" ", $fishi_string));
-	$once = false;
-	$fishi_str = null;
-	foreach ($string as $i) {
-		if ($once) {
-			$fishi_str .= "%20";
-		}
-		else {
-			$once = true;
-		}
-		$fishi_str .= $i;
-	}
+	$fishi_str = urlencode($fishi_str);
+
+	return $fishi_str;
 }
 ?>
