ce74a2877b62bbf5d78456bcc4dc5058e8f56f4f
MxAR <max.rosssner@gmail.com>
2015-10-16 22:16:56 +0200

    main
    main

---
+++ php/main.php
@@ -0,0 +1,26 @@
+<?php
+
+$fishi_str = string($_POST["grade"]);	// der string 
+$whoami = $_POST["p_m"];		// true == premium || false == 08/15
+
+$req_start = "https://api.twitter.com/1.1/";
+
+if ($whoami)
+{
+	//premium
+		
+}
+else
+{
+	// 08/15
+	
+} 
+
+function pre_pro()	// input = string || output = time_span; language (default english); 
+{
+	$fs_len = strlen($fishi_str);
+	$x = null;
+	$x = array(explode(" ", $fishi_str, 0));
+	
+}
+?>
